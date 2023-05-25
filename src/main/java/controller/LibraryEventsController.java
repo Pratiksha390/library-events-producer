3d4aa2141058936c010842d9fc53446d10a8f4eb
Pratiksha390 <92597783+Pratiksha390@users.noreply.github.com>
2023-05-25 10:07:12 +0530

    Add. LibraryEventType enum in LibraryEvent object

--- src/main/java/controller/LibraryEventsController.java
+++ src/main/java/controller/LibraryEventsController.java
@@ -2,8 +2,8 @@ package com.learnkafka.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.learnkafka.domain.LibraryEvent;
+import com.learnkafka.domain.LibraryEventType;
 import com.learnkafka.producer.LibraryEventProducer;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +16,6 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
 @RestController
-@Slf4j
 public class LibraryEventsController {
 
     @Autowired
@@ -24,11 +23,12 @@ public class LibraryEventsController {
 
     @PostMapping("/v1/libraryevent")
     public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException, TimeoutException {
-        //invoke kafka producer
-        //libraryEventProducer.sendLibraryEvent(libraryEvent);
+        libraryEvent.setLibraryEventType(LibraryEventType.NEW);
+        //libraryEventProducer.sendLibraryEvent(libraryEvent);  ---> AAPROACH 1
+        //SYNCHRONOUS APPROACH
         //SendResult<Integer,String> sendResult = libraryEventProducer.sendLibraryEventSynchronous(libraryEvent);
+        //AAPROACH 2
         libraryEventProducer.sendLibraryEvent_Approach2(libraryEvent);
-        //log.info("SendResult is {} ",sendResult.toString());
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
 }
